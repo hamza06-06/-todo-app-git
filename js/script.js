311122fdbfbd5759526c3c35ff0271e8ed65827a
hamza <hamzameksem49@gmail.com>
2025-06-12 18:16:48 +0300

    Ajout du script JS pour ajouter une tâche

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('addBtn').addEventListener('click', () => {
+  const input = document.getElementById('taskInput');
+  const task = input.value.trim();
+  if (task === '') return;
+
+  const li = document.createElement('li');
+  li.textContent = task;
+
+  document.getElementById('taskList').appendChild(li);
+  input.value = '';
+});
